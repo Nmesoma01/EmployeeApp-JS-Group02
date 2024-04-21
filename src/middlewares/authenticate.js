ab6968f632c521743072f70ef4e85d151da5783a
izzeddin62 <i.ishimwe@alustudent.com>
2024-04-21 22:59:49 +0200

    catch and return 401 when the token is not provided

--- src/middlewares/authenticate.js
+++ src/middlewares/authenticate.js
@@ -1,15 +1,19 @@
 import jwt from 'jsonwebtoken';
 
 export default function authenticate(req, res, next) {
-  const { authorization } = req.headers;
-  const token = authorization?.replace('Bearer ', '');
-  if (!token) {
+  try {
+    const { authorization } = req.headers;
+    const token = authorization?.replace('Bearer ', '');
+    if (!token) {
+      return res.status(401).send({ error: 'Unauthorized' });
+    }
+    const decoded = jwt.verify(token, process.env.SECRET);
+    if (!decoded) {
+      return res.status(401).send({ error: 'Unauthorized' });
+    }
+    req.managerId = decoded.id;
+    return next();
+  } catch (error) {
     return res.status(401).send({ error: 'Unauthorized' });
   }
-  const decoded = jwt.verify(token, process.env.SECRET);
-  if (!decoded) {
-    return res.status(401).send({ error: 'Unauthorized' });
-  }
-  req.managerId = decoded.id;
-  return next();
 }
